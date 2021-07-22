a7200c447b656f9c3bd84a6c735a557beb84da0a
misterd4109 <81401284+misterd4109@users.noreply.github.com>
2021-07-22 19:11:49 +0000

    Add files via upload

---
+++ R/djibril.R
@@ -0,0 +1,17 @@
+#'Création d'histogramme, boite à moustaphes et résumé des statistiques descriptives
+#' @export
+#' @param x variable numérique
+djibril=function(x){
+  # 1 ligne and 2 colonnes
+  par(mfrow = c(1,2))
+  #Histogramm
+  hist(x, col = rainbow(30))
+  # Boite à moustaphe
+  boxplot(x, col = 'red')
+  par(mfrow = c(1,1))
+  # Résumé numérique
+  data.frame(min=min(x),
+             median=median(x),
+             mean=mean(),
+             max=max(x))
+}
